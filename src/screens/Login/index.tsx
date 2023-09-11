c284a311a0d11b7831d7215779d13e76c5807f74
maryanahxavier <maryanahxavi9@gmail.com>
2023-09-11 12:18:51 -0300

    erros

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,63 +1,82 @@
-import React, { useState } from "react";
-import { View, KeyboardAvoidingView, Text, Alert } from "react-native";
-import { styles } from "./style";
-import { Entypo, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import { TextInput } from "react-native-gesture-handler";
-import { colors } from "../../styles/colors";
-import { ComponentButtonInterface } from "../../components";
-import { LoginTypes } from "../../Navigations/login.navigation";
-import { useAuth } from "../../hooks/auth";
-import { IAuthenticate } from "../../services/data/User";
-import { AxiosError } from "axios";
+import React, { useState, useEffect } from 'react';
+import { View, Text, KeyboardAvoidingView, TextInput, Alert} from "react-native";
+import { styles } from '././style';
+import {MaterialIcons, Entypo} from '@expo/vector-icons';
+import { colors } from '../../styles/colors';
+import {ComponentButtonInterface} from "../../components"
+import { LoginTypes } from "../../Navigations/login.navigation"
+import { useAuth } from '../../hooks/auth';
+import { IAuthenticate } from '../../services/data/User';
+import { AxiosError } from 'axios';
 
-export function Login({ navigation }: LoginTypes) {
-    const { singIn } = useAuth();
+export interface IErrorApi {
+    erros: {
+        rule: string
+        field: string
+        message: string
+    }[]
+}
+
+export function Login({navigation}: LoginTypes) {
+    const {singIn} = useAuth();
     const [data, setData] = useState<IAuthenticate>();
-    const [isLoadin, setIsLoading] = useState(true);
-    async function handleSingIn() {
-    try {
-        setIsLoading(true);
-        if (data?.email && data.passsword) {
-            await singIn(data);
-        } else {
-            Alert.alert("Preencha todos os campos !!!");
+    const [isLoading, setIsLoading] = useState(true);
+   
+    async function handleSignIn() {
+        try {
+            setIsLoading(true);
+            if(data?.email && data.passsword){
+                await singIn(data);
+            } else {
+                Alert.alert("Preencha todos os campos!");
+                setIsLoading(false);
+            }
+        }catch (error){
+            const err = error as AxiosError;
+            const message = err.response?.data as string
+            Alert.alert(message)
             setIsLoading(false);
         }
-    } catch (error) {
-        const err = error as AxiosError;
-        const message = err.response?.data as string
-        Alert.alert(message)
-        setIsLoading(false);
     }
+    function handleChange(item: IAuthenticate){
+        setData({...data, ...item})
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        },2000)
+    },[])
     return (
         <View style={styles.container}>
             <KeyboardAvoidingView>
-                <Text style={styles.title}>Login</Text>
-                <View style={styles.formRow}>
+                <Text style={styles.title} >Login</Text>
+                <View style={styles.formRow} >
                     <MaterialIcons name="email" style={styles.icon} />
                     <TextInput
-                        placeholder="email"
-                        placeholderTextColor={colors.black}
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                        style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor={colors.secondary}
+                    keyboardType="email-address"
+                    autoCapitalize='none'
+                    style={styles.input} 
                     />
+
                 </View>
-                <View style={styles.formRow}>
-                    <Entypo name="key" style={styles.icon} />
-                    <TextInput
-                        placeholder="senha"
-                        placeholderTextColor={colors.black}
-                        secureTextEntry={true}
-                        autoCapitalize="none"
-                        style={styles.input}
+                <View style={styles.formRow} >
+                <Entypo name="key" style={styles.icon}/>
+                <TextInput
+                    placeholder='Senha'
+                    placeholderTextColor={colors.secondary}
+                   secureTextEntry={true}
+                    autoCapitalize='none'
+                    style={styles.input} 
                     />
+        
+
                 </View>
-                <ComponentButtonInterface title="Entrar" type="primary" onPressI= {handleSingIn} />
-                <ComponentButtonInterface title="Cadastrar" type="primary" onPressI={() => { navigation.navigate('Cadastrar') }} />
-            </KeyboardAvoidingView>
+                <ComponentButtonInterface title="Entrar" type="primary" onPressI={handleSignIn} />
+                <ComponentButtonInterface title="Cadastre-se" type="primary" onPressI={()=>{navigation.navigate("Cadastrar")}} />
+            </KeyboardAvoidingView> 
         </View>
     )
-
-}
+} 
