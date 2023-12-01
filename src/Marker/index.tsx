15f0796ab9e74bec1777099c53bfcad3e94a1e79
maryanahxavier <maryanahxavi9@gmail.com>
2023-12-01 13:00:10 -0300

    lagrimas

--- src/Marker/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-import  { Styles } from '.styles';
-import { styles } from './styles';
-
-export function Marker() {
-    return (
-        <View style={styles.text} />
-    )
-}
--- src/Navigations/tab.navigation.tsx
+++ src/Navigations/tab.navigation.tsx
@@ -1,57 +1,59 @@
 import React from 'react';
 import { BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SecreenPerfil,SreenCamera,SreenLocation} from "../screens"
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {SecreenPerfil,SreenCamera, SreenLocation, ScreenAcelerometer} from "../screens"
 import { colors } from '../styles/colors';
-import { Ionicons, AntDesign, Entypo} from '@expo/vector-icons';
-type TabParamList = {
+import { Ionicons, AntDesign, FontAwesome } from '@expo/vector-icons';
+type TabParamList ={
   Perfil: undefined
   Camera: undefined
   Location: undefined
+  Acelerometro: undefined
 }
 type TabScreenNavigationProp = BottomTabNavigationProp<TabParamList, 'Perfil'>
 export type TabTypes = {
   navigation: TabScreenNavigationProp
 }
- export function TabNavigation() {
-       const Tab = createBottomTabNavigator<TabParamList>();
+export function TabNavigation() {
+  const Tab = createBottomTabNavigator<TabParamList>();
   return (
-    <Tab.Navigator
+    <Tab.Navigator 
       screenOptions={{
         headerStyle: {
-        backgroundColor: Colors.primary
+          backgroundColor: colors.primary
         },
         headerTintColor: colors.white,
-        tabBarActiveBackgroundColor: colors.primary,
+        tabBarActiveBackgroundColor: colors.primary, 
         tabBarActiveTintColor: colors.white
       }}
     >
       <Tab.Screen name="Perfil" component={SecreenPerfil} 
-         options={{
-           tabBarIcon: () => (
-           <Ionicons name='person' color={colors.white} size={24} />
-           ) 
-         }}
-      />
-      <Tab.Screen name="Camera" component={SreenCamera}
-      options={{
-        tabBarIcon: () => (
+        options={{
+          tabBarIcon: () => (
           <Ionicons name='person' color={colors.white} size={24} />
-          ) 
+          )
         }}
       />
-
-      <Tab.Screen name='Location' component={SreenLocation}
+      <Tab.Screen name="Camera" component={SreenCamera} 
       options={{
         tabBarIcon: () => (
-          <Entypo name="location" size={24} color="black" />
+        <AntDesign name='camera' color={colors.white} size={24} />
         )
       }}
       />
-
-
-      
+     <Tab.Screen name="Location" component={SreenLocation}
+     options={{
+      tabBarIcon: () => (
+        <FontAwesome name="map-marker" size={24} color="black" />
+      )
+     }}
+    />
+    <Tab.Screen name="Acelerometro" component={ScreenAcelerometer} 
+      options={{
+        tabBarIcon: () => (
+        <AntDesign name="car" size={24} color="black"  />
+        )
+      }}
+      />
     </Tab.Navigator>
-  
-  )
+  );
 }
